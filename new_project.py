c0e5ee437882e75c2dae096ffe037a03648f9d79
Milda Auglyte <auglyte.m@gmail.com>
2023-04-13 10:20:41 +0300

    Vabalų projektas

--- new_project.py
+++ new_project.py
@@ -1,1 +1,26 @@
-print("Labas")
+
+#Naujas projektas vabalui atpažinti
+print("Sveiki prisijungę prie programėlės, kuri jums padės atpažinti jūsų matytą vabalą!")
+
+#Paprašome įvesti vabalo spalvą ir specifikacijas
+choice_klase = input("Pasirinkite spalvą: 1 - žalias, 2 - mėlynas, 3 - raudonas: ")
+if choice_klase == "1":
+    choice_specifikacija = input("Pasirinkite vabalo specifikacijas: 1 - ilgos antenos, 2 - kvadratiškas kūnas: ")
+    if choice_specifikacija == "1":
+        print("Jūsų matytas vabalas: Muskusinis kvapūnas")
+    if choice_specifikacija == "2":
+        print("Jūsų matytas vabalas: Paprastasis auksavabalis")
+elif choice_klase == "2":
+    choice_specifikacija = input("Pasirinkite vabalo specifikacijas: 1 - pailgas ir ryškiai segmentuotas kūnas, 2 - apvalus kūnas: ")
+    if choice_specifikacija == "1":
+        print("Jūsų matytas vabalas: Paprastasis gegužvabalis")
+    if choice_specifikacija == "2":
+        print("Jūsų matytas vabalas: Paprastasis mėšlavabalis")
+elif choice_klase == "3":
+    choice_specifikacija = input("Pasirinkite vabalo specifikacijas: 1 - juodai ir raudonai dryžuotas kūnas, 2 - nedidelis ir skleidžia cypimą primenančius garsus: ")
+    if choice_specifikacija == "1":
+        print("Jūsų matytas vabalas: Juostuotoji skydblakė")
+    if choice_specifikacija == "2":
+        print("Jūsų matytas vabalas: Lelijinis lapagraužis")
+else:
+    print("Pasirinkimas neteisingas")
